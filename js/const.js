19b17e33d76e050e3916d2f1fee737fa50b96fe9
ShunkiKinoshita <latest.patrush@gmail.com>
2023-06-13 23:07:16 +0900

    不足分のコミット

--- js/const.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.sideBarMask = exports.sidebar = void 0;
-exports.sidebar = document.querySelector('.sidebar');
-exports.sideBarMask = document.querySelector('.sideBarMask');
